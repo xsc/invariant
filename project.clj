48ba4f2ac561164570439b172aeeb87910af2352
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-01 17:39:56 +0100

    remove specter utility namespace.

--- project.clj
+++ project.clj
@@ -17,6 +17,6 @@
                    :source-uri "https://github.com/xsc/invariant/blob/master/{filepath}#L{line}"
                    :namespaces [invariant.core
                                 invariant.core.protocols
-                                invariant.specter]}}}
+                                invariant.spec]}}}
   :aliases {"codox" ["with-profile" "+codox" "codox"]}
   :pedantic? :abort)
