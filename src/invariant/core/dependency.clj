5022215d6cd1fbb57667902cec3c9f331f09b2c1
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-02 17:35:21 +0100

    make 'with' non-cumulative.
    Subsequent usages with the same key will recreate the state, not extend
    it.

--- src/invariant/core/dependency.clj
+++ src/invariant/core/dependency.clj
@@ -2,10 +2,10 @@
   (:require [invariant.core.protocols :refer :all]
             [com.rpl.specter :as specter]))
 
-(deftype Dependency [invariant k path reduce-fn]
+(deftype Dependency [invariant k path reduce-fn initial-value]
   Invariant
   (run-invariant [_ path' state value]
-    (reduce
-      #(update-in %1 [:state k] reduce-fn %2)
-      (run-invariant invariant path' state value)
-      (specter/select path value))))
+    (let [result (run-invariant invariant path' state value)]
+      (->> (specter/traverse path value)
+           (reduce reduce-fn initial-value)
+           (assoc-in result [:state k])))))
