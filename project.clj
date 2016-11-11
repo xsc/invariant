d9173ad6a8049d74f9dfab1ba231c86bbc9e1d51
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-11 14:21:41 +0100

    upgrade 'specter' dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :year 2016
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
-                 [com.rpl/specter "0.13.0"]]
+                 [com.rpl/specter "0.13.1"]]
   :profiles
   {:codox {:dependencies [[codox-theme-rdash "0.1.1"]]
            :plugins [[lein-codox "0.10.0"]]
